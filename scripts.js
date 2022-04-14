880c8a7e3c3d2d31a89f5285e2b80f1f4a6b6880
luizdriven22@driven.com <luizdriven22@driven.com>
2022-04-14 17:54:27 -0300

    Carregar mensagens do servidor e exibi-las conforme layout

---
+++ scripts.js
@@ -0,0 +1,36 @@
+let chatHistory;
+
+getChatHistory();
+
+function getChatHistory() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(styleMessages);
+}
+
+function styleMessages(messages) {
+    chatHistory = messages.data;
+    const chat = document.querySelector(".chat");
+
+    for(let i = 0; i < chatHistory.length; i++) {
+
+        const type = chatHistory[i].type;
+        const time = chatHistory[i].time;
+        const from = chatHistory[i].from;
+        const to = chatHistory[i].to;
+        const text = chatHistory[i].text;
+
+        switch(type) {
+            case "status":
+                chat.innerHTML += `<div class="chat-msgs status"><p>${time} <em>${from}</em> ${text}</p></div>`;
+                break;
+
+            case "message":
+                chat.innerHTML += `<div class="chat-msgs message"><p>${time} <em>${from}</em> para <em>${to}</em>: ${text}</p></div>`;
+                break;
+
+            case "private_message":
+                chat.innerHTML += `<div class="chat-msgs private"><p>${time} <em>${from}</em> reservadamente para <em>${to}</em>: ${text}</p></div>`;
+                break;
+        }
+    }
+}
